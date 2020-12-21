7f5671da6a3e9259cc19f4817e75409565288edf
Idreessyed1 <Idreesmsyed1@gmail.com>
2020-12-20 19:21:51 -0500

    Add files via upload
    Basic implementation of backup timer.

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[]args) throws InterruptedException {
+        Sleeper sleeper = new Sleeper(1000);
+        sleeper.startSleeper();
+        while (true){
+            Thread.sleep(100);
+            System.out.println("HELLO");
+        }
+
+
+    }
+
+}
---
+++ Sleeper.java
@@ -0,0 +1,32 @@
+public class Sleeper {
+
+    private int time;
+
+    public Sleeper(int time){
+        this.time = time;
+
+    }
+
+    public void startSleeper(){
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    runBackupProcess();
+                } catch (InterruptedException e) {
+                    System.out.println("Run Backup failed");
+                }
+
+            }
+        });
+        t1.start();
+    }
+
+    public void runBackupProcess() throws InterruptedException {
+        while(true){
+            System.out.println("BACKUP");
+            Thread.sleep(time);
+
+        }
+    }
+}
